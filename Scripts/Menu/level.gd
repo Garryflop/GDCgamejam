54d761e3f630d4c73977c79a0438c1af3545e3fb
Harryfloppa <onlykrutoy009@gmail.com>
2025-04-06 10:42:51 +0500

    +sound +insDialogs

--- Scripts/Menu/level.gd
+++ Scripts/Menu/level.gd
@@ -8,13 +8,3 @@ func _on_pause_button_released() -> void:
 	var current_menu = pause_menu_packed.instantiate()
 	get_tree().current_scene.call_deferred("add_child", current_menu)
 	get_tree().paused = true
-
-
-func _on_zone_body_entered(body: Node3D) -> void:
-	if body.name == "Player":
-		$Console.start("res://Resources/Dialogs/dialogText1.txt")
-
-
-func _on_zone_2_body_entered(body: Node3D) -> void:
-	if body.name == "Player":
-		$Console.start("res://Resources/Dialogs/dialogText2.txt")
---
+++ Scripts/dialog_zone.gd
@@ -0,0 +1,7 @@
+extends Area3D
+
+@export_file("*.txt") var file: String
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		get_parent().get_parent().get_node("Console").start(file)
