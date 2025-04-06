46a8505f5ef3d9cb06a51c7be6cd76eed54bc497
Harryfloppa <onlykrutoy009@gmail.com>
2025-04-06 15:34:57 +0500

    +floor button

---
+++ Scripts/physics_button.gd
@@ -0,0 +1,15 @@
+extends StaticBody3D
+
+@onready var anim = $AnimationPlayer
+
+var pressed_sfx = preload("res://Assets/Audio/SFX/step.mp3")
+
+func _on_zone_body_entered(body: Node3D) -> void:
+	if body.is_in_group("rigids"):
+		AudioManager.play_sfx(pressed_sfx)
+		anim.play("pressed")
+
+
+func _on_zone_body_exited(body: Node3D) -> void:
+	if body.is_in_group("rigids"):
+		anim.play("notpressed")
