258cbc66a73bed2fa3ac7ae34b9f0da49ad69767
Nokkenee <nokkenall@gmail.com>
2025-04-08 23:47:46 +0500

    STUL

--- Scripts/aaaatack.gd
+++ Scripts/aaaatack.gd
@@ -5,5 +5,11 @@ var attck : bool = false
 func _physics_process(delta: float) -> void:
 	if(global_position.distance_to(player.global_position) < 5.0):
 		attck = true
-	if(attck):
-		set_linear_velocity(global_position.direction_to(player.global_position))		
+	if(attck && player.spring_arm_pivot.pickedObject != self):
+		set_linear_velocity(global_position.direction_to(player.global_position)*1.5)		
+		set_linear_velocity(Vector3(linear_velocity.x, 0, linear_velocity.z))
+	if(global_position.y < 10):
+		dead()	
+	
+func dead():
+	pass
