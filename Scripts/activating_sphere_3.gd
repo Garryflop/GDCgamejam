d7efa65525b308ba9193cbe0c459220839871c30
Nokkenee <nokkenall@gmail.com>
2025-04-08 22:09:45 +0500

    ЫАЫАЫАЫЫЫЫЫЫЫЫЫЫ

--- Scripts/activating_sphere_3.gd
+++ Scripts/activating_sphere_3.gd
@@ -1,14 +1,12 @@
 extends RigidBody3D
 var looking : bool = false
-const LOADING_SCREEN = preload("res://Scenes/Levels/level2.tscn")
-@export_file("*.tscn") var next_scene : String
+const LOADING_SCREEN = preload("res://Scenes/Menu/loading_screen.tscn")
+@export_file("*.tscn") var main_menu_scene : String
 
 func _physics_process(delta: float) -> void:
 	if(looking && (global_position.z>60.1 || global_position.z < 3.3)):
-		get_tree().change_scene(next_scene)
-		
-		
 		#var loading_scene = LOADING_SCREEN.instantiate()
-		#loading_scene.next_scene = next_scene
+		#loading_scene.next_scene = main_menu_scene
 		#add_child(loading_scene)
-		#pass
+		
+		get_tree().change_scene_to_file(main_menu_scene)
