cf191fa42228298dd61d9d70bbdc5d7b78ea059e
Harryfloppa <onlykrutoy009@gmail.com>
2025-04-09 00:47:11 +0500

    patched loading screen

--- Scripts/activating_sphere_3.gd
+++ Scripts/activating_sphere_3.gd
@@ -2,13 +2,15 @@ extends RigidBody3D
 var looking : bool = false
 const LOADING_SCREEN = preload("res://Scenes/Menu/loading_screen.tscn")
 @export_file("*.tscn") var main_menu_scene : String
+var is_activated : bool = false
 
 func _physics_process(delta: float) -> void:
-	if(looking && (global_position.z>60.1 || global_position.z < 3.3)):
-		#var loading_scene = LOADING_SCREEN.instantiate()
-		#loading_scene.next_scene = main_menu_scene
-		#add_child(loading_scene)
+	if(looking && (global_position.z>60.1 || global_position.z < 3.3)) && !is_activated:
+		is_activated = true
+		var loading_scene = LOADING_SCREEN.instantiate()
+		loading_scene.next_scene = main_menu_scene
+		add_child(loading_scene)
 		
-		
-		
-		get_tree().change_scene_to_file(main_menu_scene)
+		#
+		#
+		#get_tree().change_scene_to_file(main_menu_scene)
